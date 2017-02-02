1289431e3b9dee9c07829032bc01e96199166ca6
Traun Leyden <traun.leyden@gmail.com>
2017-02-02 00:11:56 -0800

    Add cloudformationStartInstancesCommand

--- cmd/awsutil/main.go
+++ cmd/awsutil/main.go
@@ -19,6 +19,10 @@ func main() {
 		"cloudformation stop-instances": func() (cli.Command, error) {
 			return &cloudformationStopInstancesCommand{}, nil
 		},
+		"cloudformation start-instances": func() (cli.Command, error) {
+			return &cloudformationStartInstancesCommand{}, nil
+		},
+
 	}
 
 	exitStatus, err := c.Run()
@@ -83,6 +87,73 @@ func (c *cloudformationStopInstancesCommand) Help() string {
 Usage: cloudformation stop-instances [options]
 
   Stops all instances in the given cloudformation stack
+
+Options:
+
+  -stackname=stackname      The name of the cloudformation stack
+  -region=region            The AWS region
+
+ `
+	return strings.TrimSpace(helpText)
+
+}
+
+type cloudformationStartInstancesCommand struct {
+	args []string
+}
+
+func (c *cloudformationStartInstancesCommand) Run(args []string) int {
+
+	// Parse args + get the name of the stack
+	cmdFlags := flag.NewFlagSet("cloudformation", flag.ContinueOnError)
+	cmdFlags.Usage = func() { c.Help() }
+	stackname := cmdFlags.String("stackname", "", "")
+	region := cmdFlags.String("region", "", "")
+	if err := cmdFlags.Parse(args); err != nil {
+		return 1
+	}
+
+	if *region == "" {
+		log.Fatalf("You must pass an AWS region")
+		return 1
+	}
+
+	if *stackname == "" {
+		log.Fatalf("You must pass a CloudFormation stack name")
+		return 1
+	}
+
+	// create a session
+	cfnUtil, err := awsutil.NewCloudformationUtilFromRegion(*region)
+	if err != nil {
+		log.Fatalf("Error creating cnf: %v", err)
+		return 1
+	}
+
+	// Start/restart all instances in stack
+	err = cfnUtil.StartEC2Instances(*stackname)
+	if err != nil {
+		log.Fatalf("Error starting instances for stack: %v.  Err: %v", *stackname, err)
+		return 1
+	}
+
+	log.Printf("Started all instances in stack: %v", *stackname)
+	return 0
+
+}
+
+
+
+func (c *cloudformationStartInstancesCommand) Synopsis() string {
+	return "Start all instances in given cloudformation stack"
+}
+
+func (c *cloudformationStartInstancesCommand) Help() string {
+
+	helpText := `
+Usage: cloudformation start-instances [options]
+
+  Starts/restarts all instances in the given cloudformation stack
 
 Options:
 
